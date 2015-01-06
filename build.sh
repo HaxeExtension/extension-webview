4c74c2ff4370f90f50d18ea3187ae7508f84f275
Federico Bricker <fbricker@gmail.com>
2015-01-06 00:32:08 -0300

    Fix build for arm64, x86_64 on iOS.

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 dir=`dirname "$0"`
 cd "$dir"
 project/build.sh
+rm -rf project/obj
+lime rebuild . ios
+rm -rf project/obj
 rm -f openfl-webview.zip
 zip -0r openfl-webview.zip extension dependencies assets haxelib.json include.xml project ndll android 
