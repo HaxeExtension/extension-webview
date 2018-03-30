c2e477eb414a9d0368d51d81572ee70b60212912
madneon <bartek.bobko@gmail.com>
2018-03-30 13:00:25 +0200

    Lime update.

--- extension/webview/WebView.hx
+++ extension/webview/WebView.hx
@@ -105,9 +105,9 @@ class WebView  {
 		initialized = true;
 		try {
 			#if android
-			_open = openfl.utils.JNI.createStaticMethod("extensions/webview/WebViewExtension", "open", "(Ljava/lang/String;)V");
-			_openHtml = openfl.utils.JNI.createStaticMethod("extensions/webview/WebViewExtension", "openHtml", "(Ljava/lang/String;)V");
-			var _callbackFunc = openfl.utils.JNI.createStaticMethod("extensions/webview/WebViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
+			_open = lime.system.JNI.createStaticMethod("extensions/webview/WebViewExtension", "open", "(Ljava/lang/String;)V");
+			_openHtml = lime.system.JNI.createStaticMethod("extensions/webview/WebViewExtension", "openHtml", "(Ljava/lang/String;)V");
+			var _callbackFunc = lime.system.JNI.createStaticMethod("extensions/webview/WebViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
 			_callbackFunc(new AndroidCallbackHelper());
 
 			#elseif ios
